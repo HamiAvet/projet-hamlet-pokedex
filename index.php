b7d1ab77ae6ff920b0b645df63595abbab4d1ca0
HamiAvet <hamov2003@gmail.com>
2026-05-04 16:50:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,33 @@
 <?php
-    echo "Hello World!";
-    echo "This is a simple PHP script.";
+    try {
+        // Initialiser le DSN (Data Source Name) pour la connexion à la base de données
+        $dns = 'mysql:host=gateway01.eu-central-1.prod.aws.tidbcloud.com;port=4000;dbname=hamlet_pokedex';
+
+        $options = [
+            PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false, // Désactive la vérification du certificat SSL
+            PDO::MYSQL_ATTR_SSL_CA => true, // Active l'utilisation de SSL
+        ];
+
+        // L'utilisateur de la base de données
+        $utilisateur = '2ze5GKmBxBG67a2.root';
+
+        // Mot de passe de la base de données (vide par défaut)
+        $motDePasse = 'ImECCfYlRahJ4JqB';
+        
+        // Création de la connexion PDO
+        $connection = new PDO($dns, $utilisateur, $motDePasse, $options);
+
+    } catch (Exception $ex) {
+        // Affichage de l'erreur de connexion
+        echo "Erreur de connexion à la base de données : {$ex->getMessage()}";
+    }
+
+    $select = $connection->query("SELECT * FROM pokemon");
+    $pokemons = $select->fetchAll(PDO::FETCH_ASSOC);
+    // Affiche les données en html
+    foreach ($pokemons as $pokemon) {
+        echo "<h1>{$pokemon['pokemon_id']}</h1>";
+        echo "<h2>{$pokemon['pokemon_nom']}</h2>";
+
+    }
 ?>
