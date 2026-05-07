5d0757dd43634f7732891ab05c581da84fb458df
HamiAvet <hamov2003@gmail.com>
2026-05-07 10:38:59 +0200

    Add .env file

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
+    // Chargement des variables d'environnement à partir du fichier .env
+    require_once __DIR__ . '/vendor/autoload.php';
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
     try {
         // Initialiser le DSN (Data Source Name) pour la connexion à la base de données
-        $dns = 'mysql:host=gateway01.eu-central-1.prod.aws.tidbcloud.com;port=4000;dbname=hamlet_pokedex';
-
+        $dns = "mysql:host={$_ENV['hote']};port={$_ENV['port']};dbname={$_ENV['base_de_donnees']}";
         // Options de connexion pour PDO
         $options = [
             PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false, // Désactive la vérification du certificat SSL
@@ -10,10 +13,10 @@
         ];
 
         // L'utilisateur de la base de données
-        $utilisateur = '2ze5GKmBxBG67a2.root';
+        $utilisateur = $_ENV['utilisateur'];
 
         // Mot de passe de la base de données (vide par défaut)
-        $motDePasse = 'ImECCfYlRahJ4JqB';
+        $motDePasse = $_ENV['mot_de_passe'];
         
         // Création de la connexion PDO
         $connection = new PDO($dns, $utilisateur, $motDePasse, $options);
@@ -29,6 +32,13 @@
 
     // Affiche les données en html
     foreach ($pokemons as $pokemon) {
-        echo "<h1>{$pokemon['pokemon_id']} : {$pokemon['pokemon_nom']}</h1>";
+        echo "
+        <div>
+            <img src='{$pokemon['pokemon_img']}' alt='Image de {$pokemon['pokemon_nom']}'>
+            <h1>N° {$pokemon['pokemon_id']} : {$pokemon['pokemon_nom']}</h1>
+            <h3>Poids : {$pokemon['pokemon_poids']}, Taille : {$pokemon['pokemon_taille']}</h3>
+            <p>Description : {$pokemon['pokemon_description']}</p>
+
+        </div>";
     }
 ?>
